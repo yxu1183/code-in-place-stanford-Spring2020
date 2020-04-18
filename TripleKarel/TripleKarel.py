89c6399a8bb2d337555cb59aacf385ab8edd5e89
iamyunika <yupadhayaya08@gmail.com>
2020-04-18 16:04:43 -0500

    fixed minor bug and added comments

--- TripleKarel/TripleKarel.py
+++ TripleKarel/TripleKarel.py
@@ -1,24 +1,45 @@
+"""
+Date Created: 04/18/2020
+Name: Yunika Upadhayaya
+"""
+
 from karel.stanfordkarel import *
 
 """
 File: TripleKarel.py
 --------------------
-When you finish writing this file, TripleKarel should be
-able to paint the exterior of three buildings in a given
-world, as described in the Assignment 1 handout. You
-should make sure that your program works for all of the 
-Triple sample worlds.
+TripleKarel paints the exterior of three buildings in a given
+world, as described in the Assignment 1 handout. Program works 
+for all of the Triple sample worlds.
 """
+
+
+def turn_around():
+    """
+    This function turns the beep around.
+    """
+    for i in range(2):
+        turn_left()
+
+
+def turn_right():
+    """
+    This function turns the beeper right.
+    """
+    for i in range(3):
+        turn_left()
 
 
 def check_put_beeper():
     """
-    This function puts the beeper
+    This function puts the beeper along the sides of the building and
+    moves forward.
     """
     put_beeper()
     turn_right()
     move()
     turn_left()
+
 
 def make_building():
     """
@@ -33,19 +54,6 @@ def make_building():
         move()
 
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-
-    for i in range(3):
-        make_building()
-        adjacent()
-
-
 def adjacent():
     """
     This function moves the karel to the adjacent building.
@@ -54,20 +62,20 @@ def adjacent():
     move()
 
 
-def turn_around():
-    for i in range(2):
-        turn_left()
+def main():
+    """
+    Karel completes the desired work by calling the appropriate
+    function in the main.
 
-
-def turn_right():
+    """
     for i in range(3):
-        turn_left()
+        make_building()
+        adjacent()
+    turn_left()
 
-
-
-
-
-# There is no need to edit code beyond this point
 
 if __name__ == "__main__":
+    """
+    This if statement opens up the Karel space when calling main().
+    """
     run_karel_program()
