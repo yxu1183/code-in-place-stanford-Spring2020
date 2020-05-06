7227431973b846b8afa1c99628c9abda21c6d0e4
iamyunika <yupadhayaya08@gmail.com>
2020-05-06 17:04:03 -0500

    fixed a print bug

--- Sandcastles/subtract_numbers.py
+++ Sandcastles/subtract_numbers.py
@@ -2,6 +2,7 @@
 Date Created: 04/23/2020
 Name: Yunika Upadhayaya
 """
+
 """
 File: subtract_numbers.py
 -------------------------
@@ -14,12 +15,12 @@ def main():
     """
     It prompts user to enter two numbers and prints its sum.
     """
-    print("The program subtracts one number from another.")
+    print("This program subtracts one number from another.")
     first_number = input("Enter first number: ")
     second_number = input("Enter second number: ")
 
     result = float(first_number) - float(second_number)
-    print("The result is", result)
+    print("The result is",result)
 
 
 if __name__ == '__main__':
